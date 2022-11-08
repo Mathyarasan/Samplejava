159ff9e782b509e6d88493c57f9c791dc884239d
Mathyarasan <100558999+Mathyarasan@users.noreply.github.com>
2022-11-08 10:48:56 +0530

    initial commit

---
+++ Simple.java
@@ -0,0 +1,11 @@
+
+public class Simple {
+
+	public static void run() {
+		// TODO Auto-generated method stub
+System.out.println("mathy");
+	}
+	public static void main(String[] args) {
+		run();
+	}
+}
